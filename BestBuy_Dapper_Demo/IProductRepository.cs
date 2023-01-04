dfa5c135e6dc4217b40575f33ee47d056570bd02
DeAdrien Hill <deadrienhill@yahoo.com>
2023-01-04 00:20:18 -0600

    completed dapper exercise

---
+++ BestBuy_Dapper_Demo/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuy_Dapper_Demo
+{
+    interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+
+        void CreateProduct(string name, double price, int categoryID);
+
+       
+
+
+       
+
+    }
+}
